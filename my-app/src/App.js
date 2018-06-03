d7bf41407aeed5662ae50363e3b8aef46e6e7e29
abmallick <abhinavmallick831@gmail.com>
2018-06-03 16:37:58 +0530

    React Router

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
 import './App.css';
-import {DISHES} from './dishes';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 
 class App extends Component {
@@ -14,9 +12,11 @@ class App extends Component {
 
   render() {
     return (
-      <div>
-        <Main/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Main/>
+        </div>
+      </BrowserRouter>
     );
   }
 }
