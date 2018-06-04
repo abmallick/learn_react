c3cb8f115362bde32bd63bee97ef5c1d662b8a72
abmallick <abhinavmallick831@gmail.com>
2018-06-04 13:15:42 +0530

    Single Page App 1

--- my-app/src/components/MainComponent.js
+++ my-app/src/components/MainComponent.js
@@ -5,7 +5,10 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
-import {DISHES} from '../dishes';
+import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 class Main extends Component {
@@ -16,6 +19,9 @@ class Main extends Component {
 
     this.state = {
        dishes : DISHES,
+       comments: COMMENTS,
+       promotions: PROMOTIONS,
+       leaders: LEADERS
      };
   }
 
@@ -24,7 +30,11 @@ class Main extends Component {
 
     const HomePage = () => {
       return(
-        <Home />
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
